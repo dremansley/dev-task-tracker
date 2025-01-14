d04789e01cd95aef0db731fc796fb23e1e91d8e7
Andre Mansley <andremansley@gmail.com>
2025-01-14 21:10:28 +0000

    Team Model creation

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    members = models.ManyToManyField(to=User, related_name="teams")
+    projects = models.ManyToManyField(to="projects.Project", related_name="teams")
+
+    def __str__(self):
+        return self.name
