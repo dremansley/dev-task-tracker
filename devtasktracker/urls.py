85492372574bbffd285c76c1d819559bf8221f8c
Andre Mansley <andremansley@gmail.com>
2025-01-14 21:13:43 +0000

    Init sprints app

--- devtasktracker/urls.py
+++ devtasktracker/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api/projects/', include('projects.urls')),
     path('api/tasks/', include('tasks.urls')),
     path('api/teams/', include('teams.urls')),
+    path('api/sprints/', include('sprints.urls')),
 ]
