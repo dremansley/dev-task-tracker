bf42c9d23bcea44495db2968bb96b366b7fce3da
Andre Mansley <andremansley@gmail.com>
2025-01-14 19:20:16 +0000

    Project Model Setup and Project Admin

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    project_start = models.DateTimeField(null=True,blank=True)
+    project_expiry = models.DateTimeField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
