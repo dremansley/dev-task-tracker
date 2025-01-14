0a88176f4131323bc74d8154a8a2929637b79469
Andre Mansley <andremansley@gmail.com>
2025-01-14 20:38:57 +0000

    Creation of Task Model for storing user created tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+
+    class Priority(models.TextChoices):
+        LOW = "LOW", "Low Priority"
+        MEDIUM = "MEDIUM", "Medium Priority"
+        HIGH = "HIGH", "High Priority"
+
+    class Status(models.TextChoices):
+        TO_DO = "TO_DO", "To Do"
+        IN_PROGRESS = "IN_PROGRESS", "In Progress"
+        FOR_QA = "FOR_QA", "For QA"
+        DONE = "DONE", "Done"
+
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    priority = models.CharField(
+        max_length=6,
+        choices=Priority.choices,
+        default=Priority.MEDIUM
+    )
+    status = models.CharField(
+        max_length=11,
+        choices=Status.choices,
+        default=Status.TO_DO
+    )
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    project = models.ForeignKey(
+        'projects.Project',
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+
+    estimated_time = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        null=True,
+        blank=True
+        )
+
+    def __str__(self):
+        return self.title
