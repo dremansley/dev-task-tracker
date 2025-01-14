0dacce6ff8a918a4ca3e6a6cb74661ff3df8ce65
Andre Mansley <andremansley@gmail.com>
2025-01-14 21:23:41 +0000

    Setup of the Sprint Model

--- sprints/models.py
+++ sprints/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+class Sprint(models.Model):
+    name = models.CharField(max_length=255)
+    project = models.ForeignKey(to="projects.Project", on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    tasks = models.ManyToManyField(to="tasks.Task", related_name="sprints")
+
+    def __str__(self):
+        return f"{self.name} - {self.project.name}"
