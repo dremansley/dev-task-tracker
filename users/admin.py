745c5b287b11c3fc18e75418355af17f83cf1569
Andre Mansley <andremansley@gmail.com>
2025-01-17 22:29:21 +0000

    Users Admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
-from users.models import CustomUser
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(CustomUser)
+CustomUser = get_user_model()
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = ("username", "first_name", "last_name", "email", "is_staff", "is_superuser", "last_login", "date_joined")
+
+    search_fields = ("username", "first_name", "last_name", "email")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups", "user_permissions")
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    filter_horizontal = ('groups', 'user_permissions')
+
+    readonly_fields = ('last_login', 'date_joined', "password")
